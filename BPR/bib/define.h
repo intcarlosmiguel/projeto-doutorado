10cc36f269ae1317015bcb982b3ff883b1302e11
Carlos Miguel <77444804+intcarlosmiguel@users.noreply.github.com>
2024-03-12 17:10:58 -0300

    Atualizacao BPR
    Atualizacao BPR

---
+++ BPR/bib/define.h
@@ -0,0 +1,3 @@
+#pragma once
+const double decresse = 0.9, min_step = 1E-8, max_step = 1.0, ftol = 0.2,X_TOLERANCIA = 0.01;
+const int MAXIMO_ITERACOES = 1000;
--- BPR/main.c
+++ BPR/main.c
@@ -5,6 +5,7 @@
 #include <math.h>
 #include "igraph.h"
 #include "bib/calc.h"
+#include "bib/define.h"
 
 #define ALPHA 0.15
 #define BETA 4
@@ -13,6 +14,7 @@ struct PARAMETERS{
     igraph_vector_t capacidade;
     igraph_vector_t cost_time;
     igraph_vector_int_t dual;
+    int L;
 };
 
 void load_MATOD(double** MATRIZ_OD,igraph_vector_int_t* fontes,igraph_vector_int_t* alvos){
@@ -46,18 +48,22 @@ void Dijkstra(igraph_t* Grafo,int* fonte,igraph_vector_int_t* alvos,igraph_vecto
     igraph_vector_int_list_init(&evecs, 0);
     
     igraph_vector_int_init(&inbound, 0);
-
-    igraph_get_shortest_paths_dijkstra(Grafo, &vecs,&evecs,*fonte,alvos_vs,pesos, IGRAPH_IN,parents,&inbound);
+    igraph_get_shortest_paths_dijkstra(Grafo, &vecs,&evecs,*fonte,igraph_vss_all(),pesos, IGRAPH_ALL,parents,&inbound);
     //igraph_vector_int_print(parents);
     igraph_vector_int_list_destroy(&vecs);
     igraph_vector_int_list_destroy(&evecs);
     igraph_vs_destroy(&alvos_vs);
-    
+    igraph_vector_int_destroy(&inbound);
 }
 
-void BPR(igraph_vector_t* tempo,struct PARAMETERS* BPR_PARAMETERS,igraph_vector_t* fluxo){
-    int L = igraph_vector_size(&BPR_PARAMETERS->capacidade);
-    for (int i = 0; i < L; i++) VECTOR(*tempo)[i] = (VECTOR(BPR_PARAMETERS->cost_time)[i])*(1 + ALPHA*pow(VECTOR(*fluxo)[i]/VECTOR(BPR_PARAMETERS->capacidade)[i],BETA));
+void BPR(igraph_vector_t* tempo,struct PARAMETERS* BPR_PARAMETERS,igraph_vector_t* fluxo,double *f){
+    double s;
+    *f = 0;
+    for (int i = 0; i < BPR_PARAMETERS->L; i++){
+        s = pow(VECTOR(*fluxo)[i]/VECTOR(BPR_PARAMETERS->capacidade)[i],BETA);
+        *f += (VECTOR(BPR_PARAMETERS->cost_time)[i] * VECTOR(*fluxo)[i]) * (1.0 + 0.03 * s);
+        VECTOR(*tempo)[i] = (VECTOR(BPR_PARAMETERS->cost_time)[i])*(1 + ALPHA*s);
+    }
 }
 
 int** init_parameters(struct PARAMETERS* BPR_PARAMETERS,igraph_vector_int_t* edges,int*N){
@@ -83,6 +89,7 @@ int** init_parameters(struct PARAMETERS* BPR_PARAMETERS,igraph_vector_int_t* edg
         igraph_vector_push_back(&BPR_PARAMETERS->capacidade,data[i][3]);
         igraph_vector_push_back(&BPR_PARAMETERS->cost_time,data[i][6]);
         igraph_vector_int_push_back(&BPR_PARAMETERS->dual,data[i][3]);
+        BPR_PARAMETERS->L += 1;
         VECTOR(pointer)[i] = -1;
         free(data[i]);
     }
@@ -101,32 +108,58 @@ int** init_parameters(struct PARAMETERS* BPR_PARAMETERS,igraph_vector_int_t* edg
     return edge_list;
 }
 
-void update_x(igraph_t *Grafo,double** MATRIZ_OD,int** edge_list,igraph_vector_int_t* DUAL,igraph_vector_t* fluxo,igraph_vector_int_t *fontes,igraph_vector_int_t *alvos, igraph_vector_t *pesos,int L){
+void update_x(igraph_t *Grafo,double** MATRIZ_OD,int** edge_list,igraph_vector_t* fluxo,igraph_vector_int_t *fontes,igraph_vector_int_t *alvos, igraph_vector_t *pesos,int L){
     int i,j,fonte,alvo,antecessor,index;
     double volume;
+    
     for ( i = 0; i < igraph_vector_int_size(fontes); i++){
         fonte = VECTOR(*fontes)[i];
         //printf("=============== FONTE: %d ===============\n",fonte);
         igraph_vector_int_t parents;
         igraph_vector_int_init(&parents, 0);
+        
         Dijkstra(Grafo,&fonte,alvos,pesos,&parents);
         for ( j = 0; j < igraph_vector_int_size(alvos); j++){
             alvo = VECTOR(*alvos)[j];
+            //printf("Alvo: %d\n",alvo);
             volume = MATRIZ_OD[fonte][alvo];
-            if(MATRIZ_OD[fonte][alvo] == 0) break;
             while (alvo != fonte){
                 antecessor = VECTOR(parents)[alvo];
                 index = find_id(antecessor,alvo,edge_list,L);
+                //printf("%d : (%d,%d) - %f\n",index,antecessor,alvo,volume);
                 VECTOR(*fluxo)[index] += volume;
-                if(VECTOR(*DUAL)[index] > 0 ){
-                    index = VECTOR(*DUAL)[index];
-                    VECTOR(*fluxo)[index] += volume;
-                }
                 alvo = antecessor;
             }
         }
         igraph_vector_int_destroy(&parents);
     }
+}
+
+double optimize(struct PARAMETERS* BPR_PARAMETERS,igraph_vector_t* fluxo,igraph_vector_t* tempo,igraph_vector_t* direcao,const double* objetivo_incial,igraph_vector_t* y){
+    double direcao_gradiente = 0, passo = 1.0;
+    int i,iteracoes = 0;
+    for ( i = 0; i < BPR_PARAMETERS->L; i++) direcao_gradiente += VECTOR(*direcao)[i]*VECTOR(*tempo)[i];
+    if(direcao_gradiente > 0){
+        printf("Gradiente não ótimo!");
+        exit(0);
+    }
+    double objetivo = 0;
+    double dgtest = ftol * direcao_gradiente;
+    
+    
+    while (true){
+        for ( i = 0; i < BPR_PARAMETERS->L; i++) VECTOR(*y)[i] = VECTOR(*fluxo)[i] - VECTOR(*direcao)[i]*passo;
+        BPR(tempo,BPR_PARAMETERS,y,&objetivo);
+        iteracoes++;
+        if (objetivo > *objetivo_incial + passo * dgtest) passo *= decresse;
+        else return objetivo;
+
+        if(passo < min_step) break;
+        if(passo > max_step) break;
+		if(iteracoes > MAXIMO_ITERACOES) break;
+
+    }
+    
 }
 
 void main(){
@@ -155,94 +188,53 @@ void main(){
     load_MATOD(MATRIZ_OD,&fontes,&alvos);
 
     igraph_t Grafo;
-    igraph_set_attribute_table(&igraph_cattribute_table);
     igraph_empty(&Grafo, N, IGRAPH_DIRECTED);
     igraph_add_edges(&Grafo, &edges, NULL);
     
-    //for ( i = 0; i < L; i++)printf("Ligação: %d: (%d,%d)\n",i,edge_list[i][0],edge_list[i][1]);
-    
-    //exit(0);
     int fonte,alvo,antecessor,index;
     igraph_vector_t volumes;
     igraph_vector_init(&volumes,L);
     igraph_vector_t direcao;
     igraph_vector_init(&direcao,L);
-
-    update_x(&Grafo,MATRIZ_OD,edge_list,&BPR_PARAMETERS.dual,&volumes,&fontes,&alvos, &BPR_PARAMETERS.cost_time, L);
     //igraph_vector_int_print(&fontes);
-    //igraph_vector_int_print(&alvos);
-    //for ( i = 0; i < L; i++) printf("%d - (%d,%d) %f\n",i+1,edge_list[i][0],edge_list[i][1],volumes[i]);
-    
-    //printf("# de Ligações: %ld\n",igraph_ecount(&Grafo));
-    double objetivo;
-    
+    update_x(&Grafo,MATRIZ_OD,edge_list,&volumes,&fontes,&alvos, &BPR_PARAMETERS.cost_time, L);
+    igraph_vector_print(&volumes);
+    int iteracoes = 0;
+    double objetivo,dx,dy,df,objetivo2;
     while(true){
-        BPR(&tempo,&BPR_PARAMETERS,&volumes);
-        update_x(&Grafo,MATRIZ_OD,edge_list,&BPR_PARAMETERS.dual,&tempo,&fontes,&alvos, &tempo, L);
-        objetivo = igraph_vector_sum(&tempo);
-        igraph_vector_scale(&volumes,-1);
-        
-        igraph_vector_add(&direcao,&volumes);
+
+        igraph_vector_t novo_fluxo;
+        igraph_vector_init(&novo_fluxo,BPR_PARAMETERS.L);
+
+        BPR(&tempo,&BPR_PARAMETERS,&volumes,&objetivo);
+
+        update_x(&Grafo,MATRIZ_OD,edge_list,&direcao,&fontes,&alvos, &tempo, L);
+
         igraph_vector_print(&direcao);
-        //igraph_vector_scale(fluxo,-1);
-        //igraph_vector_add(direcao,fluxo);
+
+        for ( i = 0; i < L; i++) VECTOR(direcao)[i] -= VECTOR(volumes)[i];
+
+        objetivo2 = optimize(&BPR_PARAMETERS,&volumes,&tempo,&direcao,&objetivo,&novo_fluxo);
+
+        dx = 0.0;
+		for(i=0;i<BPR_PARAMETERS.L;i++){
+			dy = fabs(VECTOR(volumes)[i]-VECTOR(novo_fluxo)[i]) / (fabs(VECTOR(volumes)[i]) + 1);
+			if(dx < dy) dx = dy;
+		}
+        df = (objetivo - objetivo2) / objetivo;
+
+        igraph_vector_init_copy(&volumes,&novo_fluxo);
+
+        igraph_vector_destroy(&novo_fluxo);
+
+        iteracoes++;
+        if(iteracoes > MAXIMO_ITERACOES) break;
+        if(dx < X_TOLERANCIA) break;
+        if(df < 1E-4) break;
+
         break;
     }
-    //BPR(tempo,edges,capacidade,cost_time,fluxo);
     igraph_vector_destroy(&BPR_PARAMETERS.capacidade);
     igraph_vector_destroy(&tempo);
     igraph_vector_destroy(&BPR_PARAMETERS.cost_time);
 }
-
-/*void main() {
-
-    igraph_t g;
-    igraph_vector_int_list_t vecs, evecs;
-    igraph_vector_int_t parents, inbound;
-    igraph_integer_t i;
-    igraph_real_t weights[] = { 0, 2, 1, 0, 5, 2, 1, 1, 0, 2, 2, 8, 1, 1, 3, 1, 1, 4, 2, 1 };
-    igraph_vector_t weights_vec;
-    igraph_vs_t vs;
-
-    igraph_vector_int_list_init(&vecs, 0);
-    igraph_vector_int_list_init(&evecs, 0);
-    igraph_vector_int_init(&parents, 0);
-    igraph_vector_int_init(&inbound, 0);
-
-    igraph_vs_vector_small(&vs, 0, 1, 3, 5, 2, 1, -1);
-    igraph_small(&g, 10, IGRAPH_DIRECTED,
-                 0, 1, 0, 2, 0, 3,   1, 2, 1, 4, 1, 5,
-                 2, 3, 2, 6,         3, 2, 3, 6,
-                 4, 5, 4, 7,         5, 6, 5, 8, 5, 9,
-                 7, 5, 7, 8,         8, 9,
-                 5, 2,
-                 2, 1,
-                 -1);
-
-    igraph_vector_view(&weights_vec, weights, sizeof(weights) / sizeof(weights[0]));
-    
-    printf("Vertices:\n");
-    for (i = 0; i < igraph_vector_int_list_size(&vecs); i++) {
-        igraph_vector_int_print(igraph_vector_int_list_get_ptr(&vecs, i));
-    }
-
-    printf("\nEdges:\n");
-    for (i = 0; i < igraph_vector_int_list_size(&evecs); i++) {
-        igraph_vector_int_print(igraph_vector_int_list_get_ptr(&evecs, i));
-    }
-
-    printf("\nParents:\n");
-    igraph_vector_int_print(&parents);
-
-    printf("\nInbound:\n");
-    igraph_vector_int_print(&inbound);
-
-    igraph_vector_int_list_destroy(&vecs);
-    igraph_vector_int_list_destroy(&evecs);
-    igraph_vector_int_destroy(&parents);
-    igraph_vector_int_destroy(&inbound);
-
-    igraph_vs_destroy(&vs);
-    igraph_destroy(&g);
-
-}*/
